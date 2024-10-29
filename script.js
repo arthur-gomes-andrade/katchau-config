daff0244e690c955ff5c500e999babed8c2248dc
arthur-gomes-andrade <slowex24@gmail.com>
2024-10-29 20:47:26 -0300

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function toggleDescription(descId) {
     const descriptionDiv = document.getElementById(descId);
     const arrowIcon = descriptionDiv.previousElementSibling.querySelector('.arrow');
-    
-    // Verifica se a descrição está visível
+
     const isVisible = descriptionDiv.style.display === "block";
 
-    // Oculta todas as descrições e reseta a rotação das setas
     document.querySelectorAll('.description').forEach(div => div.style.display = 'none');
     document.querySelectorAll('.arrow').forEach(arrow => arrow.style.transform = 'rotate(0deg)');
-    
-    // Mostra a descrição selecionada e rotaciona a seta se ela não estiver visível
+
     if (!isVisible) {
         descriptionDiv.style.display = "block";
         arrowIcon.style.transform = 'rotate(90deg)';
