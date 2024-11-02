397a29c22fc1abcf9aa714a2889cfe7b664cb1a6
arthur-gomes-andrade <slowex24@gmail.com>
2024-11-02 00:02:19 -0300

    script.js

--- script.js
+++ script.js
@@ -2,25 +2,23 @@ function toggleDescription(element) {
     const description = element.nextElementSibling;
     const arrow = element.querySelector('.arrow');
 
-    // Fechar outras descrições
     const allDescriptions = document.querySelectorAll('.description');
     allDescriptions.forEach((desc) => {
         if (desc !== description) {
-            desc.style.display = 'none'; // Fecha a descrição instantaneamente
-            desc.style.opacity = '0'; // Reseta a opacidade para 0
+            desc.style.display = 'none'; 
+            desc.style.opacity = '0'; 
             desc.previousElementSibling.querySelector('.arrow').style.transform = 'rotate(0deg)';
         }
     });
 
-    // Alternar a descrição atual
     if (description.style.display === 'none' || description.style.display === '') {
-        description.style.display = 'block'; // Exibe a descrição
+        description.style.display = 'block'; 
         requestAnimationFrame(() => {
-            description.style.opacity = '1'; // Abre a descrição com animação
+            description.style.opacity = '1'; 
         });
         arrow.style.transform = 'rotate(90deg)';
     } else {
-        description.style.display = 'none'; // Define como none instantaneamente
+        description.style.display = 'none'; 
         arrow.style.transform = 'rotate(0deg)';
     }
 }
